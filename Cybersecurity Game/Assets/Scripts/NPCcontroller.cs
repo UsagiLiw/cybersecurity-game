7ced5beef1ef14896d8c283fe4ec47c19cce7ba8
peammapake <34181322+peammapake@users.noreply.github.com>
2021-04-02 14:13:19 +0700

    Update NPC Controller

--- Cybersecurity Game/Assets/Scripts/NPCcontroller.cs
+++ Cybersecurity Game/Assets/Scripts/NPCcontroller.cs
@@ -4,8 +4,51 @@ using UnityEngine;
 
 public class NPCcontroller : MonoBehaviour
 {
-    public void TriggerNPCquest(Target target)
+    public static bool NPC_CEO;
+
+    public static bool NPC_Employee1;
+
+    public static bool NPC_Employee2;
+
+    public static bool NPC_Meeting;
+
+    public static bool NPC_IT;
+
+    private void Start()
     {
-        
+        DisableAllNPC();
+    }
+
+    public static void TriggerNPCquest(Target target)
+    {
+        switch (target)
+        {
+            case Target.CEO:
+                NPC_CEO = true;
+                break;
+            case Target.Employee1:
+                NPC_Employee1 = true;
+                break;
+            case Target.Employee1:
+                NPC_Employee2 = true;
+                break;
+            case Target.Meeting:
+                NPC_Meeting = true;
+                break;
+            case Target.IT:
+                NPC_IT = true;
+                break;
+            default:
+                Debug.Log("Warning - target not an exisitng NPC");
+        }
+    }
+
+    public static void DisableAllNPC()
+    {
+        NPC_CEO = false;
+        NPC_Employee1 = false;
+        NPC_Employee2 = false;
+        NPC_Meeting = false;
+        NPC_IT = false;
     }
 }
