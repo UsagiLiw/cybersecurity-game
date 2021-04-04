3f20b7f082c1cf89439a868e019eecafefcfb6fb
Mill Nonthakorn <34181310+UsagiLiw@users.noreply.github.com>
2021-04-04 20:57:38 +0700

    Update ShopManager.cs

--- Cybersecurity Game/Assets/Scripts/ShopManager.cs
+++ Cybersecurity Game/Assets/Scripts/ShopManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void BuyItem(int index)
     {
         Debug.Log("Buy item index " + index);
