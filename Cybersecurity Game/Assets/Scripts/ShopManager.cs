9af95673b643bd8956f729ac53d4a340c08838bf
Mill Nonthakorn <34181310+UsagiLiw@users.noreply.github.com>
2021-04-04 18:06:45 +0700

    create shop manager
    Only blank function with debug

---
+++ Cybersecurity Game/Assets/Scripts/ShopManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BuyItem(int index)
+    {
+        Debug.Log("Buy item index " + index);
+    }
+}
