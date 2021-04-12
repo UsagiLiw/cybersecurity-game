2be21b7cd4b381d9ccd18e3c8114b429de9f5039
Mill Nonthakorn <34181310+UsagiLiw@users.noreply.github.com>
2021-04-12 17:34:27 +0700

    Update NewsManager.cs
    NOT FINISHED
    need to know what scenario is active

--- Cybersecurity Game/Assets/Scripts/NewsManager.cs
+++ Cybersecurity Game/Assets/Scripts/NewsManager.cs
@@ -2,21 +2,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class NewsManager : MonoBehaviour
 {
-    private static EmailObject[] newsDict;
+    private static News[] newsDict;
+
+    private News[] newsArray = new News[3];
 
     [SerializeField] private GameManager gameManager;
 
+
+    
     void Start()
     {
+        SetNewsDictionary();
         gameManager.DayPassed += UpdateNews;
     }
 
-    public void SetDictionaries()
+    private void UpdateNews()
     {
-        SetNewsDictionary();
+        for (int i = 0; i < newsArray.Length; i++)
+        {
+            int templateLength = newsDict.Length - 1;
+            int rIndex = Random.Range(0, templateLength);
+
+            newsArray[i] = newsDict[rIndex];
+        }
+
+    }
+
+    //Overload method
+    private void SetNews(string scenarioId)
+    {
+        for (int i = 0; i < newsArray.Length; i++)
+        {
+            int templateLength = newsDict.Length - 1;
+            int rIndex = Random.Range(0, templateLength);
+
+            newsArray[i] = newsDict[rIndex];
+
+            if (i == 3)
+            {
+                //logic !! find template that match the scenarioId
+                foreach(News news in newsDict)
+                {
+                    if(news.template.Equals(scenarioId))
+                    {
+                        newsArray[i] = news;
+                    }
+                }
+            }
+        }
+
     }
 
     private void SetNewsDictionary()
@@ -27,12 +65,7 @@ public class NewsManager : MonoBehaviour
             Debug.Log("Error - Unable to find EmailTemplate.json");
             Application.Quit();
         }
-        newsDict = JsonHelper.FromJson<EmailObject>(jsonString);
-    }
-
-    private void UpdateNews()
-    {
-        throw new NotImplementedException();
+        newsDict = JsonHelper.FromJson<News>(jsonString);
     }
 }
 
@@ -43,5 +76,6 @@ public class News
     public int scenario;
     public string topic;
     public string content;
+    public string image;
 
 }
