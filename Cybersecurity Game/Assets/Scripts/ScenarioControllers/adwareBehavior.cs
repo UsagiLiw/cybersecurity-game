867a3cb8e473a2e20278ecc8ed521eb9cc066493
peammapake <34181322+peammapake@users.noreply.github.com>
2021-04-22 15:16:02 +0700

    Implementing adware desktop

--- Cybersecurity Game/Assets/Scripts/ScenarioControllers/adwareBehavior.cs
+++ Cybersecurity Game/Assets/Scripts/ScenarioControllers/adwareBehavior.cs
@@ -1,11 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using Random = UnityEngine.Random;
 
 public class adwareBehavior : MonoBehaviour
 {
-    private static string adPics_path = "";
-    private void OnEnable() {
-        
+    public DesktopAdsClass[] desktopAds;
+
+    public GameObject notiBox;
+
+    public GameObject notiImage;
+
+    private static string adsImage_Path = "AdsDesktop/";
+
+    private void OnEnable()
+    {
+        int index = Random.Range(0, desktopAds.Length);
+
+        notiBox.SetActive(false);
+        notiImage.SetActive(false);
+
+        if (string.IsNullOrEmpty(desktopAds[index].adsFile))
+        {
+            ShowNotiBox(desktopAds[index]);
+        }
+        else
+        {
+            ShowNotiImage(desktopAds[index]);
+        }
     }
+
+    private void ShowNotiBox(DesktopAdsClass currentAds)
+    {
+        notiBox.SetActive(true);
+
+        Text topic = notiBox.transform.GetChild(1).gameObject.transform.GetComponent<Text>();
+        Text content = notiBox.transform.GetChild(2).gameObject.transform.GetComponent<Text>();
+        topic.text = currentAds.adsHeader;
+        content.text = currentAds.adsDetail;
+    }
+
+    private void ShowNotiImage(DesktopAdsClass currentAds)
+    {
+        notiImage.SetActive(true);
+
+        Text topic = notiImage.transform.GetChild(1).gameObject.transform.GetComponent<Text>();
+        Image content = notiBox.transform.GetChild(2).gameObject.transform.GetComponent<Image>();
+        topic.text = currentAds.adsHeader;
+        content.sprite = Resources.Load<Sprite>(adsImage_Path + currentAds.adsFile);
+    }
+
+    public void SelfDestruct()
+    {
+        Destroy(this.gameObject);
+    }
+}
+
+[System.Serializable]
+public class DesktopAdsClass
+{
+    public string adsHeader;
+
+    public string adsDetail;
+
+    public string adsFile;
 }
