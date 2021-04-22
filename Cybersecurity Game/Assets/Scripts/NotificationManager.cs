2a32c825f72ef42009efc2d6278d658231be4f3d
Mill Nonthakorn <34181310+UsagiLiw@users.noreply.github.com>
2021-04-22 18:48:16 +0700

    Add NotificationManager

---
+++ Cybersecurity Game/Assets/Scripts/NotificationManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationManager : MonoBehaviour
+{
+    public List<Notification> notificationList = new List<Notification>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class Notification
+{
+    public string sender;
+    public string room;
+    public string content;
+}
