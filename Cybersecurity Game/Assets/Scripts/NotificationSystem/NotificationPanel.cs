e539479a12d123965dd1d0e5ab4c7c1a9d54eab4
Mill Nonthakorn <34181310+UsagiLiw@users.noreply.github.com>
2021-04-26 20:45:25 +0700

    A lot of work see detail
    - create prefab of each noti
    - create script NotificationPanel
    - adjust a lot of thing
    - move scripts location

---
+++ Cybersecurity Game/Assets/Scripts/NotificationSystem/NotificationPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationPanel : MonoBehaviour
+{
+    [SerializeField] private Text sender;
+    [SerializeField] private Text detail;
+
+    public Text Sender { get => sender; set => sender = value; }
+    public Text Detail { get => detail; set => detail = value; }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
