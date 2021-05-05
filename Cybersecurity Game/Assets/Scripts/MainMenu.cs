b11339f876d2f18badc5e542972567ac5302ebf7
Mill Nonthakorn <34181310+UsagiLiw@users.noreply.github.com>
2021-05-05 15:40:43 +0700

    add script mainmenu

---
+++ Cybersecurity Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        Debug.Log("Start new game");
+        //SceneManager.LoadScene("MainScene");
+    }
+
+    public void ContinueGame()
+    {
+        Debug.Log("Continue game");
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
